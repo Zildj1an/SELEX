fc5fd1fe9d982992bcb368025db7406e68e4e2ed
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2019-07-12 17:49:58 +0200

    Add files via upload

--- SELEX.py
+++ SELEX.py
@@ -5,7 +5,8 @@
 
 """
 
-from opentrons import labware, instruments, modules
+from opentrons import labware, instruments, modules, robot
+import time
 
 metadata = {
 	'protocolName' : 'Selex',
@@ -16,7 +17,7 @@ metadata = {
 
 
 # [1] Labware
-# TODO: Comprobar types, confirmar posición.
+# TODO: Comprobar types, confirmar posición, thermic module, Ninja-PCR.
 
 plate_name = 'Ninja-PCR'
 if plate_name not in labware.list():
@@ -29,11 +30,11 @@ if plate_name not in labware.list():
         volume=200)
 
 plate_samples    =   labware.load('96-flat', '11')			            # Samples
-tiprack          =   labware.load('opentrons-tiprack-10ul', '8')        # Tipracks
+tiprack          =   labware.load('opentrons-tiprack-10ul', '8')        	    # Tipracks
 magnetic         =   modules.load('magdeck','1')			            # Magnetic Deck
-plate_magnet     =   labware.load('96-flat', '1', share = True)		    # Magnetic Deck plate
-thermocicler     =   modules.load('Ninja-PCR','10') 					# Ninja-PCR
-thermic_module   =   modules.load()					                    # Auxiliar thermic module
+plate_magnet     =   labware.load('96-flat', '1', share = True)		            # Magnetic Deck plate
+thermocicler     =   modules.load('Ninja-PCR','10') 			            # Ninja-PCR
+thermic_module   =   modules.load()					            # Auxiliar thermic module
 
 # [2] Pipettes
 # TODO:
@@ -44,3 +45,13 @@ pipette = instruments.P50_Single(mount = 'left', tip_racks = [tiprack])
 #TODO:
 
 # Ejemplo: pipette.trasnfer(100, plate.wells('A1'), plate.wells('B1'))
+
+# When done
+while true:
+        robot._driver.turn_off_button_light()
+	time.sleep(.500)
+        robot._driver.turn_on_button_light()
+
+
+
+
