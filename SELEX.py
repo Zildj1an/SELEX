65c82bfe8bfb680743acfeb661138e6782a4b76b
Zildj1an <cbilbao@ucm.es>
2019-07-16 13:53:31 +0200

    problem with array

--- SELEX.py
+++ SELEX.py
@@ -16,27 +16,27 @@ metadata = {
 }
 
 # [1] Labware
-# TODO: Comprobar types, confirmar position, {thermic module, Ninja-PCR} sizes.
+# TODO: Comprobar types, depths.
 
 pcr_name = 'Ninja-PCR'
 if pcr_name not in labware.list():
     thermocicler = labware.create(
         pcr_name,                       # Labware Name
         grid=(4, 4),                    # Amount of (columns, rows)
-        spacing=(12, 12),               # Distances (mm) between each (column, row)
-        diameter=5,                     # Diameter (mm) of each well on the plate
+        spacing=(9, 9),                 # Distances (mm) between each (column, row)
+        diameter=2,                     # Diameter (mm) of each well on the plate
         depth=10,                       # Depth (mm) of each well on the plate
-        volume=200)
+        volume=50)
 
 thermic_name = 'Thermic_Module'
 if thermic_name not in labware.list():
     thermic = labware.create(
         thermic_name,                   # Labware Name
         grid=(4, 4),                    # Amount of (columns, rows)
-        spacing=(12, 12),               # Distances (mm) between each (column, row)
-        diameter=5,                     # Diameter (mm) of each well on the plate
+        spacing=(9, 9),                 # Distances (mm) between each (column, row)
+        diameter=2,                     # Diameter (mm) of each well on the plate
         depth=10,                       # Depth (mm) of each well on the plate
-        volume=200)
+        volume=50)
 
 plate_samples    =   labware.load('96-flat',      slot ='11')  			    # Samples
 tiprack          =   labware.load('tiprack-10ul', slot='6')	         	    # Tipracks
@@ -47,22 +47,39 @@ thermic_module   =   labware.load(thermic_name,   slot ='3')			    # Auxiliar th
 
 # [2] Pipettes
 
-pipette_l = instruments.P50_Single(mount = 'left', tip_racks=[tiprack])
-pipette_r = instruments.P50_Single(mount = 'right', tip_racks=[tiprack])
+pipette_l   = instruments.P50_Single(mount = 'left', tip_racks=[tiprack])
+pipette_r  = instruments.P50_Multi(mount = 'right', tip_racks=[tiprack])
 
 # [3] Commands
 # TODO usar ambos pipettes
 
 def samples_to_pcr():
+
         robot._driver.turn_on_rail_lights()
 	# Empezar a calentar a 90 grados
         pipette_l.pick_up_tip()
-        pipette_l.aspirate(50, plate_samples.wells('A1'))
-        pipette_l.dispense(50, plate_samples.wells('B1'))
+
+        for x in range(0,7):
+              pipette_r.aspirate(50, plate_samples.wells(x))
+
+        pipette_l.aspirate(50, plate_samples.wells(8))
+
+        for x in range(0,7):
+              pipette_r.dispense(50, thermocicler.wells(x))
+
+        pipette_l.dispense(50,thermocicler.wells(8))
+
+        # More
+
         pipette_l.return_tip()
+
+        for x in range(0,7):
+                pipette_r[x].return_tip()
+
         robot._driver.turn_off_rail_lights()
 
 def samples_to_aux():
+
        robot._driver.turn_on_rail_lights()
        pipette_l.pick_up_tip()
        # aspirar de pcr, dispensar en thermocicler
