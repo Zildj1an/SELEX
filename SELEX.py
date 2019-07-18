aba85a9fd7a59fbcd5f4057cb8d16faf910295ef
Zildj1an <cbilbao@ucm.es>
2019-07-18 13:23:32 +0200

    more aux

--- SELEX.py
+++ SELEX.py
@@ -8,7 +8,7 @@
 from opentrons import labware, instruments, modules, robot
 from subprocess import Popen
 from time import sleep
-import os, requests
+import os, requests, logging
 
 metadata = {
 	'protocolName' : 'Selex',
@@ -49,7 +49,7 @@ plate_samples    =   labware.load('96-flat',      slot ='11')  			    # Samples
 tiprack          =   labware.load('tiprack-10ul', slot='6')	         	    # Tipracks
 magnetic         =   modules.load('magdeck',      slot ='1')	                    # Magnetic Deck
 plate_magnet     =   labware.load('96-flat',      slot ='1', share = True)	    # Magnetic Deck plate
-thermocicler     =   labware.load(pcr_name,       slot ='10')			    # Ninja-PCR
+thermocycler     =   labware.load(pcr_name,       slot ='10')			    # Ninja-PCR
 thermic_module   =   labware.load(thermic_name,   slot ='3')			    # Auxiliar thermic module
 trash            =   labware.load('trash-box',    slot = '12', share = True)        # Trash
 
@@ -66,7 +66,7 @@ def init_move(function):
         # button turns red
         robot._driver.turn_on_rail_lights()
         robot._driver.turn_on_red_button_light()
-        cmd  = "ffplay -nodisp -autoexit /root/robot.mp3 &> /dev/null"
+        cmd  = "ffplay -nodisp -autoexit /mnt/usbdrive/robot.mp3 &> /dev/null"
         cmd2 = "pkill ffplay"
 
         # Will play while door is opened
@@ -90,32 +90,25 @@ def samples_to_pcr():
 
         pipette_r.pick_up_tip()
 
-	# Aspirate 4
-        pipette_r.aspirate(50,plate_samples.wells('E1'))
-	# Dispense 4/16
-        pipette_r.dispense(50,thermocicler.wells('A1'))
-        # Aspirate 4
-        pipette_r.aspirate(50,plate_samples.wells('E2'))
-	# Dispensed 8/16
-        pipette_r.dispense(50,thermocicler.wells('A2'))
-        # Aspirate 4
-        pipette_r.aspirate(50,plate_samples.wells('E3'))
-	# Dispensed 12/16
-        pipette_r.dispense(50,thermocicler.wells('A3'))
-        # Aspirate 4
-        pipette_r.aspirate(50,plate_samples.wells('E4'))
-	# Dispensed 16/16
-        pipette_r.dispense(50,thermocicler.wells('A4'))
+        for x,y in [('E1','A1'),('E2','A2'),('E3','A3'),('E4','A4')]:
+
+         	# Aspirate 4
+                pipette_r.aspirate(50,plate_samples.wells(x))
+	        # Dispense 4
+                pipette_r.dispense(50,thermocycler.wells(y))
 
         pipette_r.drop_tip()
 
 def samples_to_aux():
 
-       robot._driver.turn_on_rail_lights()
-       pipette_l.pick_up_tip()
-       # aspirar de pcr, dispensar en thermocicler
-       pipette_l.return_tip()
-       robot._driver.turn_off_rail_lights()
+       pipette_r.pick_up_tip()
+
+       for x in ['A1','A2','A3','A4']:
+
+             pipette_r.aspirate(50,thermocycler.wells(x))
+             pipette_r.dispense(50,thermic_module.wells(x))
+
+       pipette_r.drop_tip()
 
 def api_request(temp,URL):
 
@@ -129,6 +122,8 @@ def api_request(temp,URL):
 
 # [4] SELEX execution
 
+logging.getLogger("opentrons").setLevel(logging.INFO)
+
 # Warming at 90 degrees for 600 seconds
 
 print("Applying heat to samples...\n")
@@ -140,11 +135,11 @@ init_move(samples_to_pcr)
 print("Moving samples to cool them...\n")
 #api_request(4, URL_AUX)
 # TODO cerrar la puerta
-#sleep(600)
-#init_move(samples_to_aux)
+# TODO sleep 10 mins
+sleep(1)
+init_move(samples_to_aux)
 
 # ... (TODO)
 
 robot._driver.home()
-
 
