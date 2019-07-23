d293109198629bf9cc8cd1b2b4d0f221caf1bc29
pablo-vs <pavill01@ucm.es>
2019-07-23 17:00:06 +0200

    Modified pcr driver

--- ninjapcr.py
+++ ninjapcr.py
@@ -11,16 +11,16 @@ def NinjaPCR(Object):
     Opentrons driver for NinjaPCR
     """
 
-    PCR_NAME="NinjaPCR"
+    PCR_NAME = "NinjaPCR"
     
-    CONNECTION_MODES=["external_wifi", "ninja_ap", "opentrons_ap"]
+    CONNECTION_MODES = ["external_wifi", "ninja_ap", "opentrons_ap"]
     
     def __init__(self,
-                 slot="10",
-                 name="ninjapcr",
-                 connection_mode="external_wifi",
-                 ssid="NinjaPCR"
-                 psk="ninjapcr"):
+                 slot = "10",
+                 name = "ninjapcr",
+                 connection_mode = "external_wifi",
+                 ssid = None,
+                 psk = None):
 
         if connection_mode not in CONNECTION_MODES:
             raise ValueError("Invalid connection mode: {}".format(connection_mode))
@@ -44,13 +44,14 @@ def NinjaPCR(Object):
 
     def connect(self):
 
-        if connection_mode == "external_wifi":
-            self._check_connection()
-
-        elif connection_mode == "ninja_ap":
+        if connection_mode == "ninja_ap":
             self._connect_to_ap()
             
-        else:
+        elif connection_mode == "opentrons_ap":
+            self._setup_ap()
+
+        self._check_connection()
+
 
     def _check_connection(self):
         # Test HTTP connection
@@ -61,16 +62,29 @@ def NinjaPCR(Object):
         else:
             raise Error("Could not connect to NinjaPCR at {}".format(self.local_url))
 
+        
     def _connect_to_ap(self):
+        
         # Connect to the NinjaPCR AP
         
         if nmcli.connection_exists(self.ssid):
             self._check_connection()
 
         else:
+            if self.ssid not in nmcli.available_ssids():
+                raise Error("Network {} not found".format(self.ssid))
+                            
             success, msg = nmcli.configure(self.ssid, nmcli.WPA_PSK, self.psk)
             if not success:
                 raise Error("Could not setup connection: {}".format(msg))
+                            
             sleep(2)
-            if not 
+            if not nmcli.connection_exists(self.ssid):
+                raise Error("Could not connect to ssid {}. Wrong passphrase?".format(self.ssid))
             
+            
+    def _setup_ap(self):
+
+        # Setup the robot's wifi interface as an access point
+
+        nmcli._call(['nmcli', 'dev', 'wifi', 'hotspot', 'ifname', 'wlan0', 'ssid', self.ssid, 'password', self.psk]
