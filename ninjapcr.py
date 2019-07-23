8620f37c6940c5efb6fdc5b920cbd506ae4960fb
pablo-vs <pavill01@ucm.es>
2019-07-23 18:10:52 +0200

    NinjaPCR driver:  + Added support for sending commands and example program

--- ninjapcr.py
+++ ninjapcr.py
@@ -5,16 +5,52 @@ from time import sleep
 
 log = logging.getLogger('opentrons')
 
+PCR_NAME = "NinjaPCR"
+    
+CONNECTION_MODES = ["external_wifi", "ninja_ap", "opentrons_ap"]
+    
+COMMAND_DIR = "/command"
+
+SAMPLE_PROGRAM = {'name': 'Sample program',
+                  'lid_temp': 110,
+                  'steps': [
+                      {'type': 'step',
+                       'time': 30,
+                       'temp': 95,
+                       'name': 'Initial Step',
+                       'ramp': 0},
+                      {'type': 'cycle',
+                       'count': 2,
+                       'steps': [
+                           {'type': 'step',
+                            'time': 30,
+                            'temp': 95,
+                            'name': 'Denaturing',
+                            'ramp': 0},
+                           {'type': 'step',
+                            'time': 30,
+                            'temp': 55,
+                            'name': 'Annealing',
+                            'ramp': 0},
+                           {'type': 'step',
+                            'time': 60,
+                            'temp': 72,
+                            'name': 'Extending',
+                            'ramp': 0}
+                       ]},
+                      {'type': 'step',
+                       'time': 0,
+                       'temp': 20,
+                       'name': 'Final Hold',
+                       'ramp': 0}
+                      ]}
+
 def NinjaPCR(Object):
 
     """
     Opentrons driver for NinjaPCR
     """
 
-    PCR_NAME = "NinjaPCR"
-    
-    CONNECTION_MODES = ["external_wifi", "ninja_ap", "opentrons_ap"]
-    
     def __init__(self,
                  slot = "10",
                  name = "ninjapcr",
@@ -28,6 +64,7 @@ def NinjaPCR(Object):
         self.local_url = "http://" + name + ".local"
         self.net_ssid = ssid
         self.net_psk = psk
+        self.connected = False
         
         # Define and load associated labware
         
@@ -42,8 +79,88 @@ def NinjaPCR(Object):
         labware.load(pcr_name, slot = slot)
 
 
-    def connect(self):
+    def send_command(self, cmd, program = None):
 
+        # Send an HTTP API request to the NinjaPCR with the given command
+        # When cmd == 'start', a program in dict form must be specified
+
+        if not self.connected:
+            raise Error('NinjaPCR is not connected')
+        
+
+        params = self._build_params(cmd, program)
+
+        response = requests.get(self.local_url + COMMAND_DIR, params=params)
+
+        if response and response.status_code == 200:
+            # We got a response
+            if '"1"' not in response.text:
+                # Unsuccesful command
+                raise Error(f'Could not execute command {cmd}'
+
+        else:
+            raise Error(f'Bad connection: {response.status_code}')
+        
+
+
+    def _build_params(self, cmd, program):
+
+        # Prepare the parameters for the HTTP API request
+
+        params = {'s': 'ACGTC',
+                  'c': cmd}
+
+        if cmd == "start":
+            params['l'] = program['lid_temp']
+
+            # Build program string
+
+            prog_string = ""
+
+            for i in range(len(program['steps'])):
+
+                p = program['steps'][i]
+                
+                if p['type'] == 'step':
+                    # If this is a step, add (1..) when necessary and build string
+                    if i == 0 or program['steps'][i-1]['type'] == 'cycle':
+                        prog_string += '(1{})'
+                    else:
+                        prog_string += '{}'
+
+                    prog_string.format(self._build_step_string(p))
+
+                else:
+                    # If this is a cycle, just build it
+
+                    prog_string += self._build_step_string(p)
+
+            params['p'] = prog_string
+                    
+        return params
+
+    def _build_step_string(self, program):
+        
+        # Build the command string for this step
+
+        step_string = ""
+
+        if program['type'] == 'step':
+            p = program
+            step_string = '[{}|{}|{}|{}]'.format(p['time'], p['temp'], p['name'], p['ramp'])
+
+        else:
+            step_string = '({}'.format(program['count'])
+            for s in program['steps']:
+                step_string += "[{}|{}|{}|{}]".format(s['time'], s['temp'], s['name'], s['ramp'])
+            step_string += ')'
+
+        return step_string
+
+    
+    def connect(self):
+        # Attempt to connect to the NinjaPCR. This should be called before attempting to send any command
+        
         if connection_mode == "ninja_ap":
             self._connect_to_ap()
             
@@ -51,26 +168,23 @@ def NinjaPCR(Object):
             self._setup_ap()
 
         self._check_connection()
-
+        self.connected = True
 
     def _check_connection(self):
         # Test HTTP connection
         
         response = requests.get(self.local_url)
-        if response.status_code == 200:
+        if response and response.status_code == 200:
             log.info("Connected to NinjaPCR at {}".format(self.local_url))
         else:
-            raise Error("Could not connect to NinjaPCR at {}".format(self.local_url))
+            raise Error("Could not connect to NinjaPCR at {}. Status code: {}".format(self.local_url, response.status_code))
 
         
     def _connect_to_ap(self):
         
         # Connect to the NinjaPCR AP
-        
-        if nmcli.connection_exists(self.ssid):
-            self._check_connection()
-
-        else:
+    
+        if not nmcli.connection_exists(self.ssid):
             if self.ssid not in nmcli.available_ssids():
                 raise Error("Network {} not found".format(self.ssid))
                             
@@ -84,6 +198,8 @@ def NinjaPCR(Object):
             
             
     def _setup_ap(self):
+
+        raise NotImplementedError()
 
         # Setup the robot's wifi interface as an access point
 
