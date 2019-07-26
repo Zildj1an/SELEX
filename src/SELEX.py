08e351811124f67b14d25a59b57203b543f108bc
Zildj1an <cbilbao@ucm.es>
2019-07-26 12:10:32 +0200

    DNA Amplification

--- src/SELEX.py
+++ src/SELEX.py
@@ -6,6 +6,7 @@
 """
 
 from opentrons import labware, instruments, modules, robot
+from ninjapcr import NinjaPCR
 from subprocess import Popen
 from time import sleep
 import os, requests, logging, sys
@@ -23,17 +24,6 @@ URL_PCR = "http://ninjapcr.local/command"
 URL_AUX = "http://thermaux.local/command"
 
 # [1] Labware
-# TODO: Check types.
-
-pcr_name = 'Ninja-PCR'
-if pcr_name not in labware.list():
-    thermocicler = labware.create(
-        pcr_name,                       # Labware Name
-        grid=(4, 4),                    # Amount of (columns, rows)
-        spacing=(9, 9),                 # Distances (mm) between each (column, row)
-        diameter=2,                     # Diameter (mm) of each well on the plate
-        depth=10,                       # Depth (mm) of each well on the plate
-        volume=50)
 
 thermic_name = 'Thermic_Module'
 if thermic_name not in labware.list():
@@ -49,7 +39,7 @@ plate_samples    =   labware.load('96-flat',      slot ='11')  			    # Samples
 tiprack          =   labware.load('tiprack-10ul', slot='6')	         	    # Tipracks
 magnetic         =   modules.load('magdeck',      slot ='4')	                    # Magnetic Deck
 plate_magnet     =   labware.load('96-flat',      slot ='4', share = True)	    # Magnetic Deck plate
-thermocycler     =   labware.load(pcr_name,       slot ='10')			    # Ninja-PCR
+thermocycler     =   NinjaPCR(slot='10', simulating = robot.is_simulating())	    # Ninja-PCR
 thermic_module   =   labware.load(thermic_name,   slot ='3')			    # Auxiliar thermic module
 trash            =   labware.load('trash-box',    slot = '12', share = True)        # Trash
 
@@ -60,7 +50,12 @@ pipette_r   = instruments.P50_Multi(mount = 'right', tip_racks=[tiprack], trash_
 
 # [3] Commands
 
-def execute_move(function):
+def execute_move(function, args):
+
+        # For the simulation
+        if robot.is_simulating():
+                function(*args)
+                return
 
         # When the robot starts moving light goes on and
         # button turns red
@@ -79,12 +74,20 @@ def execute_move(function):
                if robot._driver.read_window_switches() and p.poll() is None:
                       os.system(cmd2)
         try:
-           function()
+           function(*args)
         except:
            print("Error calling function " + function().__name__ + "\n")
 
         robot._driver.turn_off_rail_lights()
         robot._driver.turn_on_blue_button_light()
+
+def next_loc(loc):
+        if int(loc[1:]) < 12:
+                n_loc = loc[0] + str(int(loc[1:])+1)
+        else:
+                n_loc = loc
+                n_loc[0] -= 1
+        return n_loc
 
 def samples_to_pcr():
 
@@ -95,7 +98,7 @@ def samples_to_pcr():
          	# Aspirate 4
                 pipette_r.aspirate(50,plate_samples.wells(x))
 	        # Dispense 4
-                pipette_r.dispense(50,thermocycler.wells(y))
+                pipette_r.dispense(50,thermocycler.labware.wells(y))
 
         pipette_r.drop_tip()
 
@@ -105,33 +108,60 @@ def samples_to_aux():
 
        for x in ['A1','A2','A3','A4']:
 
-             pipette_r.aspirate(50,thermocycler.wells(x))
+             pipette_r.aspirate(50,thermocycler.labware.wells(x))
              pipette_r.dispense(50,thermic_module.wells(x))
 
        pipette_r.drop_tip()
 
-def api_request(temp, URL):
+def DNA_amplification(plate, pipette_r, tiprack, thermocycler, primer_well, mm_well, dna_well, water_well, first_mix, second_mix, third_mix):
+       
+        location = 'H1'
+        pipette_r.pick_up_tip(location = tiprack.wells(location))
+        volumes = [5,25,20, [20, 10]]
+        dispense_m = {primer_well:[first_mix,second_mix],mm_well:[first_mix,second_mix,third_mix],dna_well:[second_mix],water_well:[first_mix,third_mix]}
 
-         command = '(1[1500|' + temp + '|Final Hold|0])'
-         PARAMS = {'s': 'ACGTC',
-                   'c':'start',
-                   'l': temp, 			# Ignored by aux therm
-                   'p': command}
-         try:
-             data = requests.get(url = URL, params = PARAMS)
-             print(data)
-         except requests.exceptions.HTTPError as e:
-             print("Error at request: " + str(e))
-             sys.exit()
+        # (1) MasterMix
+        for sample in [mm_well,water_well, primer_well, dna_well]:
+                
+                 pipette_r.pick_up_tip(location = tiprack.wells(location))
+	         location = next_loc(location)
+
+                 for wells in dispense_m[sample]:
+                   
+                     pipette_r.aspirate(volumes[1],plate.wells(sample).botton(1))
+                     pipette_r.dispense(volumes[1],plate.wells(wells))
+                 pipette_r.drop_tip()
+
+        pipette_r.transfer(50, plate.wells('A1'), thermocycler.labware.wells('A1'))
+        location = next_loc(location)
+        pipette_r.pick_up_tip(location = tiprack.wells(location))
+        pipette_r.transfer(50, plate.wells('B1'), thermocycler.labware.wells('A2'))
+        location = next_loc(location)
+        pipette_r.pick_up_tip(location = tiprack.wells(location))
+        pipette_r.transfer(50, plate.wells('C1'), thermocycler.labware.wells('A3'))
 
 # [4] SELEX execution
 
 logging.getLogger("opentrons").setLevel(logging.INFO)
 
+# Attempt to connect to the thermocycler
+if not thermocycler.connected:
+        thermocycler.connect()
+
 # (1) Warming at 90 degrees for 600 seconds (PCR)
 
 print("Applying heat to samples...\n")
-#api_request('90',URL_PCR)
+
+heat_program = {'name': 'Heat',
+                'lid_temp': 110,
+                'steps': [{
+                    'type': 'step',
+                    'time': 1500,
+                    'temp': 90,
+                    'name': 'Initial Step',
+                    'ramp': 0}]}
+
+thermocycler.send_command('start',heat_program)
 execute_move(samples_to_pcr)
 
 # (2) Cooling at 4 degrees for 600 seconds (aux)
@@ -145,7 +175,7 @@ execute_move(samples_to_aux)
 
 # (3)
 
-#api_request(,URL_AUX) TODO a que temp?
+#api_request(,URL_AUX)
 #sleep(1 hour)
 
 # (4) Magnentic separation
@@ -160,6 +190,20 @@ execute_move(samples_to_aux)
 # (6)
 
 # (7)
+
+# ...
+
+# Amplify the DNA via PCR
+
+primer_well = 'A2'
+mm_well     = 'B2'
+dna_well    = 'C2'
+water_well  = 'D2'
+first_mix   = 'A1'
+second_mix  = 'B1'
+third_mix   = 'C1'
+execute_move(pcr, [plate, pipette_r, tiprack, ninja, primer_well, mm_well, dna_well, water_well, first_mix, second_mix, third_mix])
+
 
 robot._driver.home()
 
