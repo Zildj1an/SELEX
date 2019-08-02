7a4ad71621844d05eb451d98e38a3b73b0ff692a
pablo-vs <pavill01@ucm.es>
2019-08-02 09:21:22 +0200

    Updated protocols

--- src/DNA_amplification.py
+++ src/DNA_amplification.py
@@ -17,7 +17,7 @@ FINAL_HOLD = {'name': 'Final hold',
                          'temp': 20,
                          'name': 'Final Hold',
                          'ramp': 0}]}
-
+'''
 def next_loc(loc):
         if int(loc[1:]) < 12:
                 n_loc = loc[0] + str(int(loc[1:])+1)
@@ -25,39 +25,42 @@ def next_loc(loc):
                 n_loc = loc
                 n_loc[0] -= 1
         return n_loc
+'''
 
-def pcr(plate, pipette_r, tiprack, thermocycler, primer_well, mm_well, dna_well, water_well, first_mix, second_mix, third_mix):
+def pcr(plate, pipette, tiprack, thermocycler, primer_well, mm_well, dna_well, water_well, first_mix, second_mix, third_mix):
         # Attempt to connect to the thermocycler
 
-        if not thermocycler.connected:
-                thermocycler.connect()
+        #if not thermocycler.connected:
+        #        thermocycler.connect()
 
-        location = 'H1'
-        pipette_r.pick_up_tip(location = tiprack.wells(location))
+        #location = 'H1'
+        #pipette_r.pick_up_tip(location = tiprack.wells(location))
         volumes = [5,25,20, [20, 10]]
         dispense_m = {primer_well:[first_mix,second_mix],mm_well:[first_mix,second_mix,third_mix],dna_well:[second_mix],water_well:[first_mix,third_mix]}
 
         # (1) MasterMix
         for sample in [mm_well,water_well, primer_well, dna_well]:
                 
-                 pipette_r.pick_up_tip(location = tiprack.wells(location))
-	         location = next_loc(location)
+                #pipette.pick_up_tip(location = tiprack.wells(location))
+                #location = next_loc(location)
+                pipette.pick_up_tip()
+                
+                for wells in dispense_m[sample]:
+                        
+                        pipette.aspirate(volumes[1],plate.wells(sample).bottom(1))
+                        pipette.dispense(volumes[1],plate.wells(wells))
 
-                 for wells in dispense_m[sample]:
-                   
-                     pipette_r.aspirate(volumes[1],plate.wells(sample).botton(1))
-                     pipette_r.dispense(volumes[1],plate.wells(wells))
-                 pipette_r.drop_tip()
-
-        #pipette_r.pick_up_tip(location = tiprack.wells(location))
-
-        pipette_r.transfer(50, plate.wells('A1'), thermocycler.labware.wells('A1'))
-        location = next_loc(location)
-        pipette_r.pick_up_tip(location = tiprack.wells(location))
-        pipette_r.transfer(50, plate.wells('B1'), thermocycler.labware.wells('A2'))
-        location = next_loc(location)
-        pipette_r.pick_up_tip(location = tiprack.wells(location))
-        pipette_r.transfer(50, plate.wells('C1'), thermocycler.labware.wells('A3'))
+                pipette.drop_tip()
+                        
+                #pipette.pick_up_tip(location = tiprack.wells(location))
+                        
+        pipette.transfer(50, plate.wells('A1'), thermocycler.labware.wells('A1'))
+        #location = next_loc(location)
+        #pipette.pick_up_tip(location = tiprack.wells(location))
+        pipette.transfer(50, plate.wells('B1'), thermocycler.labware.wells('A2'))
+        #location = next_loc(location)
+        #pipette.pick_up_tip(location = tiprack.wells(location))
+        pipette.transfer(50, plate.wells('C1'), thermocycler.labware.wells('A3'))
 
         PROGRAM = {'name': 'Heat',
                    'lid_temp': 110,
@@ -68,9 +71,9 @@ def pcr(plate, pipette_r, tiprack, thermocycler, primer_well, mm_well, dna_well,
                            'name': 'Heat',
                            'ramp': 0}]}
 
-        thermocycler.wait_for_program(PROGRAM)
+        #thermocycler.wait_for_program(PROGRAM)
         robot.home()
-
+'''
 def execute_move(function, args):
 
         if robot.is_simulating():
@@ -102,7 +105,7 @@ def execute_move(function, args):
 
                 robot._driver.turn_off_rail_lights()
                 robot._driver.turn_on_blue_button_light()
-
+'''
 # Run PCR as an independent protocol
 
 # Labware and module initialization
@@ -121,8 +124,10 @@ second_mix = 'B1'
 third_mix = 'C1'
 
 
-pipette_r = instruments.P50_Multi(mount='right')
+pipette_l = instruments.P50_Single(mount='left')
 
-execute_move(pcr, [plate, pipette_r, tiprack, ninja, primer_well, mm_well, dna_well, water_well, first_mix, second_mix, third_mix])
+args = [plate, pipette_l, tiprack, ninja, primer_well, mm_well, dna_well, water_well, first_mix, second_mix, third_mix]
+
+pcr(*args)
 
 
--- src/coating.py
+++ src/coating.py
@@ -23,6 +23,10 @@ metadata = {
 
 # [0] Our design for the three modules
 
+max_speed_per_axis = {
+    'x': 600, 'y': 500, 'z': 150, 'a': 150, 'b': 40, 'c': 40}
+robot.head_speed(**max_speed_per_axis)
+
 '''
 database.delete_container('Falcon_Samples')
 database.delete_container('Eppendorf_Samples')
@@ -45,7 +49,7 @@ if plate_eppendorf not in labware.list():
       grid = (8,4),
       spacing = (15,20),
       diameter = 10,
-      depth  = 32,
+      depth  = 39,
       volume = 100)
 
 # [1] Labware
@@ -60,23 +64,30 @@ plate_samples    = labware.load('96-flat',       slot = '11')                   
 
 pipette_l = instruments.P50_Single(mount = 'left', tip_racks=[tiprack], trash_container = trash)
 
-def custom_transfer(pipette,quantity,pos1,pos2,A,B):
+pipette_l.set_flow_rate(aspirate=50, dispense=100)
+
+volume_50 = 50000
+
+ml_rate = 9/5000
+
+def custom_transfer(pipette,quantity,pos1,pos2,A,B,depth=1):
    
    times = quantity // 50
    
-   pipette.pick_up_tip()
+   
 
-   for i in range(1,times):
-      pipette.aspirate(50,pos1.wells(A).bottom(1))
+   for i in range(1,times+1):
+      pipette.aspirate(50,pos1.wells(A).bottom(depth))
       pipette.dispense(50,pos2.wells(B))
+      pipette.blow_out(pos2.wells(B).top(-4))
 
    quantity = quantity - (times * 50)
 
    if quantity > 0:
-      pipette.aspirate(quantity,pos1.wells(A).bottom(1))
+      pipette.aspirate(quantity,pos1.wells(A).bottom(depth))
       pipette.dispense(quantity,pos2.wells(B))
+      pipette.blow_out(pos2.wells(B).top(-4))
       
-  
 # [3] Execution
 
 # Previous:
@@ -84,53 +95,82 @@ def custom_transfer(pipette,quantity,pos1,pos2,A,B):
 
 # (1) Each Falcon to Eppendorfs:
 
-falcon50 = 'A3'
-val = 1
+falcon50 = 'A1'
+val = 0
 
-for falcon in ['A1','B1','B2']:
+for falcon in ['B5','C5','D5']:
 
-    # 1st Eppendorf -> 400 ul of PBS + 400 ul of Falcon15
-    custom_transfer(pipette_l,400,Falcon,Eppendorf,falcon50,'A'+ str(val))
-    pipette_l.mix(2,50,Falcon.wells(falcon))
-    pipette_l.drop_tip()
-    custom_transfer(pipette_l,400,Falcon,Eppendorf,falcon,  'A' + str(val))
+   # 1st Eppendorf -> 400 ul of PBS + 400 ul of Falcon15
+   pipette_l.pick_up_tip()
+   custom_transfer(pipette_l,400,Falcon,Eppendorf,falcon50,chr(ord('A')+ val)+'1',volume_50*ml_rate)
+   volume_50 -= 400
+   pipette_l.drop_tip()
+   pipette_l.pick_up_tip()
+   # pipette_l.mix(3,50,Eppendorf.wells(falcon))
+   # RESUSPENDER EN CADA ASPIRATE-DISPENSE
+   pipette_l.transfer(400,Eppendorf.wells(falcon),Eppendorf.wells(chr(ord('A')+ val)+'1'),new_tip='never', mix_before=(3,50), blow_out=True)
+   pipette_l.drop_tip()
+   
+   # 2nd Eppendorf -> 640 ul of PBS + 160 ul of Falcon15
+   pipette_l.pick_up_tip()
+   custom_transfer(pipette_l,640,Falcon,Eppendorf,falcon50,chr(ord('A')+ val)+'2',volume_50*ml_rate)
+   volume_50 -= 640
+   pipette_l.drop_tip()
+   pipette_l.pick_up_tip()
+   #pipette_l.mix(3,50,Eppendorf.wells(falcon))
+   pipette_l.transfer(160,Eppendorf.wells(falcon),Eppendorf.wells(chr(ord('A')+ val)+'2'),new_tip='never', mix_before=(3,50), blow_out=True)
+   pipette_l.drop_tip()
+   
+   # 3rd Eppendorf -> 720 ul of PBS + 80 ul of Falcon
+   pipette_l.pick_up_tip()
+   custom_transfer(pipette_l,720,Falcon,Eppendorf,falcon50,chr(ord('A')+ val)+'3',volume_50*ml_rate)
+   volume_50 -= 720
+   pipette_l.drop_tip()
+   pipette_l.pick_up_tip()
+   #pipette_l.mix(3,50,Eppendorf.wells(falcon))
+   #custom_transfer(pipette_l,80,Eppendorf,Eppendorf,falcon,chr(ord('A')+ val)+'3')
+   pipette_l.transfer(80,Eppendorf.wells(falcon),Eppendorf.wells(chr(ord('A')+ val)+'3'),new_tip='never', mix_before=(3,50), blow_out=True)
+   pipette_l.drop_tip()
+   
+   # 3rd Eppendorf -> 784 ul of PBS + 16 ul of Falcon
+   pipette_l.pick_up_tip()
+   custom_transfer(pipette_l,784,Falcon,Eppendorf,falcon50,chr(ord('A')+ val)+'4',volume_50*ml_rate)
+   volume_50 -= 784
+   pipette_l.drop_tip()
+   pipette_l.pick_up_tip()
+   #pipette_l.mix(3,50,Eppendorf.wells(falcon))
+   #custom_transfer(pipette_l,16,Eppendorf,Eppendorf,falcon,chr(ord('A')+ val)+'4')
+   pipette_l.transfer(16,Eppendorf.wells(falcon),Eppendorf.wells(chr(ord('A')+ val)+'4'),new_tip='never', mix_before=(3,50), blow_out=True)
+   pipette_l.drop_tip()
+   
+   val += 1
 
-    # 2nd Eppendorf -> 640 ul of PBS + 160 ul of Falcon15
-    custom_transfer(pipette_l,640,Falcon,Eppendorf,falcon50,'B' + str(val))
-    pipette_l.mix(2,50,Falcon.wells(falcon))
-    pipette_l.drop_tip()
-    custom_transfer(pipette_l,160,Falcon,Eppendorf,falcon,  'B' + str(val))
 
-    # 3rd Eppendorf -> 720 ul of PBS + 80 ul of Falcon
-    custom_transfer(pipette_l,720,Falcon,Eppendorf,falcon50,'C' + str(val))
-    pipette_l.mix(2,50,Falcon.wells(falcon))
-    pipette_l.drop_tip()
-    custom_transfer(pipette_l,80,Falcon,Eppendorf,falcon,   'C' + str(val))
 
-    # 3rd Eppendorf -> 784 ul of PBS + 16 ul of Falcon
-    custom_transfer(pipette_l,784,Falcon,Eppendorf,falcon50,'D' + str(val))
-    pipette_l.mix(2,50,Falcon.wells(falcon))
-    pipette_l.drop_tip()
-    custom_transfer(pipette_l,16,Falcon,Eppendorf,falcon,   'D' + str(val))
+   
+for pos in ['A','B','C']:
+      
+   init = 1
+   end = 6
+   row = 'A'
+      
+   if pos == 'B':
+      init = 7
+      end  = 12
 
-    val = val + 1
+   if pos == 'C':
+      row = 'E'
+      
+   for j in range(1,5):
 
-    for pos in ['A','B','C','D']:
-       
-        init = 1
-        end = 6
+      pipette_l.pick_up_tip()
+      
+      for i in range(init,end+1):
 
-        if falcon == 'B1':
-           init = 1
-           end  = 6 
- 
-           
-        for i in range(init,end):
-
-           pipette_l.transfer(100,Eppendorf.wells(pos + str(val)),plate_samples.wells(pos + str(i)), new_tip='never')
-           
-        pipette_l.drop_tip()
-         
+         pipette_l.transfer(100,Eppendorf.wells(pos + str(j)),plate_samples.wells(chr(ord(row)+j-1)+str(i)), new_tip='never', mix_before=(3,50), blow_out=True)
+               
+      pipette_l.drop_tip()
+               
 
 
 robot._driver.turn_off_rail_lights()
