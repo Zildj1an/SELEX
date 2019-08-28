cba9a2b5c40423eb061b67930185a7b152eed1ba
Zildj1an <cbilbao@ucm.es>
2019-08-28 11:46:19 +0200

    tabs protocol

--- src/elona.py
+++ src/elona.py
@@ -84,9 +84,9 @@ multi_tip_loc = ['E',1]
 def pick_up_multi():
 
    global multi_tip_loc
-   
+
    pipette_r.pick_up_tip(location = tiprack_r.wells(''.join(map(str,multi_tip_loc))))
-   
+
    if multi_tip_loc[1] == 12:
       if multi_tip_loc[0] == 'A':
          raise Exception("Multichannel out of tips")
@@ -100,11 +100,11 @@ def storage_samples(where, vol, new_tip='once', module = Storage, safe_flow_rate
    # Where is an array: ['A1'] will always aspirate from that well. ['A1','A2','A3'] will transfer
    # from well A1 in module to first row in samples, A2 to second row, etc
 
-   # safe_flow_rate sets the dispense rate 
+   # safe_flow_rate sets the dispense rate
 
    if new_tip == 'once':
       pipette_r.pick_up_tip()
-      
+
    times = vol // pipette_r.max_volume
 
    vol = vol % pipette_r.max_volume
@@ -112,7 +112,7 @@ def storage_samples(where, vol, new_tip='once', module = Storage, safe_flow_rate
    if len(where) < 3:
       # Support for different origins
       where *= 3
-      
+
    for sample,origin in zip([f'A{i}' for i in range(1, 4)], where):
 
       # For every well in D1 - D3
@@ -141,7 +141,6 @@ def storage_samples(where, vol, new_tip='once', module = Storage, safe_flow_rate
       pipette_r.drop_tip()
 
    pipette_r.set_flow_rate(aspirate=flow_rate['a_r'], dispense=flow_rate['d_r'])
-   
 
 def samples_trash(vol, new_tip='once', safe_flow_rate=15, downto=0):
 
@@ -153,14 +152,14 @@ def samples_trash(vol, new_tip='once', safe_flow_rate=15, downto=0):
 
    if new_tip == 'once':
       pipette_r.pick_up_tip()
-      
+
    for sample in [f'A{i}' for i in range(1, 4)]:
 
       if new_tip == 'always':
          pipette_r.pick_up_tip()
-         
+
       for x in range(1,times+1):
-         
+
          pipette_r.aspirate(pipette_r.max_volume,plate_samples.wells(sample).bottom(downto))
          pipette_r.dispense(pipette_r.max_volume,trash_liquid.wells(sample))
          pipette_r.blow_out(trash_liquid.wells(sample))
@@ -171,15 +170,15 @@ def samples_trash(vol, new_tip='once', safe_flow_rate=15, downto=0):
          pipette_r.dispense(vol,trash_liquid.wells(sample))
          pipette_r.blow_out(trash_liquid.wells(sample))
          pipette_r.touch_tip()
-         
+
       if new_tip == 'always':
          pipette_r.drop_tip()
 
    if new_tip == 'once':
       pipette_r.drop_tip()
-      
+
    pipette_r.set_flow_rate(aspirate=flow_rate['a_r'], dispense=flow_rate['d_r'])
-   
+
 def tween_wash():
 
    # (3) Lavado x3 con PBS 1x tween 0.1
@@ -192,7 +191,7 @@ def tween_wash():
          robot._driver.turn_on_red_button_light()
          while not robot._driver.read_button():
             sleep(0.5)
-            
+
          robot._driver.turn_on_blue_button_light()
 
       if x < 3:
@@ -204,7 +203,7 @@ def robot_wait():
        robot._driver.turn_on_red_button_light()
        while not robot._driver.read_button():
           sleep(0.5)
-          
+
        robot._driver.turn_on_blue_button_light()
 
 
@@ -229,9 +228,9 @@ tween_wash()
 
 for epp,dest in [('A1','A1'), ('A2','D1'), ('A2','D2'), ('A2','D3'), ('A3','A2'), ('A3','A3')]:
    # source, dest
-   
+
    pipette_l.pick_up_tip()
-   
+
    for pos in range(1,4):
       if '3' not in epp:
          # Don't mix aptamer buffer
@@ -239,7 +238,7 @@ for epp,dest in [('A1','A1'), ('A2','D1'), ('A2','D2'), ('A2','D3'), ('A3','A2')
       pipette_l.set_flow_rate(dispense=20)
       pipette_l.transfer(100,Eppendorf.wells(epp),plate_samples.wells(addrow(dest,pos-1)), new_tip='never', blow_out=True)
       pipette_l.set_flow_rate(dispense=200)
-        
+
      pipette_l.drop_tip()
 
 # Pausar para incubar 1h - PAUSE (Hand made)
@@ -251,9 +250,9 @@ tween_wash()
 # (7) Eppendorf con anticuerpo A todos (en módulo térmico!)
 
 for epp,dest in [('B1','A1'), ('B1','D1'), ('B1','A2'), ('B1','D3'), ('B2','A3'), ('B2','D2')]:
-   
+
    pipette_l.pick_up_tip()
-   
+
    for pos in range(1,4):
       if '3' not in epp:
          # Don't mix aptamer buffer
@@ -261,10 +260,9 @@ for epp,dest in [('B1','A1'), ('B1','D1'), ('B1','A2'), ('B1','D3'), ('B2','A3')
       pipette_l.set_flow_rate(dispense=20)
       pipette_l.transfer(100,Eppendorf.wells(epp),plate_samples.wells(addrow(dest,pos-1)), new_tip='never', blow_out=True)
       pipette_l.set_flow_rate(dispense=200)
-        
+
      pipette_l.drop_tip()
-     
-   
+
 # Pausar para incubar 1h - PAUSE (Hand made)
 robot_wait()
 
