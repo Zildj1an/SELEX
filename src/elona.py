672cf280705737ef5cca7fec35b1fc3acbf18007
Zildj1an <cbilbao@ucm.es>
2019-08-28 13:09:31 +0200

    error storage

--- src/elona.py
+++ src/elona.py
@@ -60,22 +60,22 @@ Eppendorf        = labware.load('Eppendorf_Samples', slot = 4)
 tiprack_l        = labware.load('opentrons-tiprack-300ul', slot=9)
 tiprack_r        = labware.load('opentrons-tiprack-10ul', slot=6)
 tiprack_r2       = labware.load('opentrons-tiprack-10ul', slot=11)
+#tiprack_r3       = labware.load('opentrons-tiprack-10ul', slot=7)
+
 # Note: tiprack_r is actually an 'opentrons-
 
 # [2] Pipettes
 
 pipette_l = instruments.P300_Single(mount = 'left', tip_racks=[tiprack_l])
-pipette_r = instruments.P50_Multi(mount = 'right', tip_racks=[tiprack_r, tiprack_r2])
+pipette_r = instruments.P50_Multi(mount = 'right', tip_racks=[tiprack_r, tiprack_r2])#,tiprack_r3])
 
 # Pipette flow rates: left and right, aspirate and dispense
 
 flow_rate = {'a_l': 200, 'd_l': 200, 'a_r': 50, 'd_r': 50}
 
-
 def addrow(well, num):
    # addrow('A1',1) -> 'B1'
    return chr(ord(well[0])+num) + well[1:]
-
 
 # Right pipette will pick up tips in this order: E1,E2...E12,A1,A2....
 # This way we can use a whole tiprack while only picking up four tips
@@ -157,16 +157,16 @@ def samples_trash(vol, new_tip='once', safe_flow_rate=15, downto=0):
 
       if new_tip == 'always':
          pipette_r.pick_up_tip()
-         pipette_r.aspirate(pipette_r.max_volume,plate_samples.wells(sample).bottom(downto))
-         pipette_r.dispense(pipette_r.max_volume,trash_liquid.wells(sample))
-         pipette_r.blow_out(trash_liquid.wells(sample))
-         pipette_r.touch_tip()
+      pipette_r.aspirate(pipette_r.max_volume,plate_samples.wells(sample).bottom(downto))
+      pipette_r.dispense(pipette_r.max_volume,trash_liquid.wells(sample))
+      pipette_r.blow_out(trash_liquid.wells(sample))
+      pipette_r.touch_tip()
 
       if vol > 0:
          pipette_r.aspirate(vol,plate_samples.wells(sample).bottom(downto))
-         pipette_r.dispense(vol,trash_liquid.wells(sample))
-         pipette_r.blow_out(trash_liquid.wells(sample))
-         pipette_r.touch_tip()
+      pipette_r.dispense(vol,trash_liquid.wells(sample))
+      pipette_r.blow_out(trash_liquid.wells(sample))
+      pipette_r.touch_tip()
 
       if new_tip == 'always':
          pipette_r.drop_tip()
@@ -192,7 +192,7 @@ def tween_wash():
          robot._driver.turn_on_blue_button_light()
 
       if x < 3:
-         samples_trash(200, new_tip='always', downto = -5)
+         samples_trash(200, new_tip='once', downto = -5)
 
 def robot_wait():
     if not robot.is_simulating():
@@ -203,13 +203,11 @@ def robot_wait():
 
        robot._driver.turn_on_blue_button_light()
 
-
 robot._driver.turn_on_rail_lights()
 pipette_l.set_flow_rate(aspirate=flow_rate['a_l'], dispense=flow_rate['d_l'])
 
 # (-2)
-samples_trash(200, new_tip='always', downto = -5)
-
+samples_trash(200, new_tip='once', downto = -5)
 tween_wash()
 
 # (1) 200ul of PBS 1x BSA 5% to plate
