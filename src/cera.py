d07d14a531d98ee0aa4bc1a1cc98dff261f36226
pablo-vs <pavill01@ucm.es>
2019-09-27 16:25:54 +0200

    Cera

--- src/cera.py
+++ src/cera.py
@@ -5,6 +5,8 @@ Applies wax to paper sheets using a custom stamp
 
 from opentrons import labware, instruments, modules, robot
 from time import sleep
+from subprocess import Popen
+import os
 
 metadata = {
    'protocolName' : 'Dar cera, pulir cera',
@@ -30,25 +32,43 @@ if paper_sheet not in labware.list():
         diameter = 5,
         depth = 2)
 
-stamps = labware.load(stamp_tiprack, slot=6)
-wax = labware.load('agilent_1_reservoir_290ml', slot=10)
+stamps = labware.load(stamp_tiprack, slot=7)
+wax = labware.load('agilent_1_reservoir_290ml', slot=4)
 paper = labware.load(paper_sheet, slot=1)
 
 pipette = instruments.P300_Single(mount='left')
 
+
+cmd  = "ffplay -nodisp -autoexit /data/cera.mp3 &> /dev/null"
+cmd2 = "pkill ffplay"
+
+if not robot.is_simulating():
+	p = Popen(cmd,shell=True)
+
 pipette.pick_up_tip(location=stamps.wells('A1'))
 
-for i in range(1,5):
-
+for j in range(1,3):
+    pipette.move_to(wax.wells('A1').top(20))
+    pipette.aspirate(30)
+    pipette.move_to(wax.wells('A1').top(-2))
+    pipette.dispense(30)
+    pipette.move_to(wax.wells('A1').top(20))
+    pipette.aspirate(30)
+    pipette.move_to(wax.wells('A1').top(-2))
+    pipette.dispense(30)
     pipette.move_to(wax.wells('A1').top(20))
     pipette.aspirate(30)
     pipette.move_to(wax.wells('A1').top(-2))
     pipette.dispense(30)
     pipette.move_to(wax.wells('A1').top(20))
 
-    pipette.move_to(paper.wells(f'A{i}').top())
-    pipette.aspirate(30)
-    pipette.dispense(30)
-    pipette.move_to(paper.wells(f'A{i}').top(-2))
+    for i in range(1,4):
+        pipette.move_to(paper.wells(f'A{i}').top())
+        pipette.aspirate(30)
+        pipette.move_to(paper.wells(f'A{i}').top(-2))
+        pipette.dispense(30)
 
 pipette.return_tip()
+
+if not robot.is_simulating():
+	os.system(cmd2)
